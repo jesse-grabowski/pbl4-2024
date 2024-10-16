26bc4f7972b33410b76b9ce47ad730b61e3add98
Jesse Grabowski <github@jessegrabowski.com>
2024-10-16 12:25:45 +0900

    Fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/ritspbl2024f/teamc/',
   plugins: [
     vue(),
     vueJsx(),
