f25d3d71544382a3fb72c40f2723721e28cf2fad
lxymahatma <lxymahatma@gmail.com>
2024-10-17 17:52:23 +0900

    add global app error handler

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { FaExpandArrowsAlt, FaCompressArrowsAlt } from "oh-vue-icons/icons";
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { FaExpandArrowsAlt, FaCompressArrowsAlt } from 'oh-vue-icons/icons'
 
 addIcons(FaExpandArrowsAlt, FaCompressArrowsAlt)
 
@@ -15,5 +15,9 @@ const app = createApp(App)
 app.component('v-icon', OhVueIcon)
 app.use(createPinia())
 app.use(router)
+
+app.config.errorHandler = (err, vm, info) => {
+  console.error('Error', err, vm, info)
+}
 
 app.mount('#app')
