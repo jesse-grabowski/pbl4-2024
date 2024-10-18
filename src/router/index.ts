e356a3788305c2f641fcbd9c605e10f4428a8538
lxymahatma <326265648@qq.com>
2024-10-18 13:45:12 +0900

    implement auto router for .vue files in views folder

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import GameView from '../views/GameView.vue'
+import { routes } from 'vue-router/auto-routes'
+
+console.log(routes)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/game',
-      name: 'game',
-      component: GameView
-    }
-  ]
+  routes,
 })
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,47 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ritspbl2024f/teamc/',
   server: {
-    port: 5500
+    port: 5500,
   },
   plugins: [
+    VueRouter({
+      routesFolder: [
+        {
+          src: 'src/views',
+          path: file => {
+            // Convert file path to route path
+            const srcPath = 'src/views'
+            const path = file.slice(file.lastIndexOf(srcPath) + srcPath.length, -8).toLocaleLowerCase()
+            return path
+          },
+        },
+      ],
+      dts: './src/router.d.ts',
+      // remove '/' prefix for route name
+      getRouteName: routeNode => routeNode.fullPath.slice(1),
+      extendRoute: route => {
+        // Add alias for home route
+        if (route.name === 'home') {
+          route.addAlias('/')
+        }
+      },
+    }),
     vue(),
     vueJsx(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   optimizeDeps: {
-    include: ['lodash.debounce']
-  }
+    include: ['lodash.debounce'],
+  },
 })
