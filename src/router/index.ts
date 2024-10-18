d45a1b6308d6fbd29a43c091bf52068458b00aef
lxymahatma <lxymahatma@gmail.com>
2024-10-18 20:11:02 +0900

    update routes at runtime without reloading the page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from 'vue-router/auto-routes'
+import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
-export default router
+if (import.meta.hot) {
+  handleHotUpdate(router)
+}
