9412488b07bf36f0d97a5a5972065b77c6e433f3
lxymahatma <lxymahatma@gmail.com>
2024-11-02 02:11:45 +0900

    format changes to imagedata, capitalize the first letter in title

--- src/data/image-data.ts
+++ src/data/image-data.ts
@@ -1,106 +1,116 @@
-import type { Image } from "@/models/image";
+import type { Image } from '@/models/image'
 
 const imageData: Image[] = [
-    {
-        "description": "The fire hydrant near the staircase on the H5 floor sits beside large windows, allowing natural light to fill the space. It combines functionality with a sense of openness. Our company office also has comfortable spaces open to public and safety equipments. You can check it here: TeamCcompany.com",
-        "floor": 5,
-        "isPanorama": true,
-        "coordinate": {lat: 34.8093582, lng: 135.5615536},
-        "title": "the fire hydrant and window in front of the stairs",
-        "url": new URL('@/assets/images/guess/panorama/p1.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
-    {
-        "description": "This outdoor area near Building H offers a peaceful open space surrounded by greenery, where students can take a break and enjoy the fresh air. Our company also values open outdoor spaces, providing similar relaxing environments at our offices. You can check it here: TeamCcompany.com",
-        "floor": 1,
-        "isPanorama": true,
-        "coordinate": {lat: 34.80898639844242, lng:135.562220669336},
-        "title": "Outside",
-        "url": new URL('@/assets/images/guess/panorama/p10.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
-    {
-        "description": "The view between the C and H buildings on the H4 floor showcases the unique layout of the campus, with greenery and open walkways that invite relaxation and reflection. Our company spaces also feature open, scenic areas for visitors to unwind. You can check it here : TeamCcompany.com",
-        "floor": 4,
-        "isPanorama": true,
-        "coordinate": {lat: 34.8094810, lng: 135.5616153},
-        "title": "in front of the laboratory and a painting",
-        "url": new URL('@/assets/images/guess/panorama/p2.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
-    {
-        "description": "The corridor outside the research rooms on C4 is adorned with colorful paintings, creating an inspiring and creative environment for learning and collaboration. Our office halls are also decorated with art pieces that inspire our team every day. You can check it here: TeamCcompany.com",
-        "floor": 4,
-        "isPanorama": true,
-        "coordinate": {lat: 34.8100302, lng: 135.5614118},
-        "title": "In front of Arena",
-        "url": new URL('@/assets/images/guess/panorama/p3.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
-    {
-        "description": "Ritsumeikan’s C2 Arena is a versatile space where students engage in physical education classes, club practices, and personal recreational activities. A hub for active learning and community building. In comparison, our company offers state-of-the-art facilities that support diverse recreational activities and team-building events. TeamCcompany.com/arena",
-        "floor": 2,
-        "isPanorama": true,
-        "coordinate": {lat: 34.8098504, lng: 135.5610534},
-        "title": "In Front of Arena",
-        "url": new URL('@/assets/images/guess/panorama/p4.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
-    {
-        "description": "The Wakebayashi Yasuhiro Memorial Room honors the life and work of Professor Wakebayashi Yasuhiro, a dedicated scholar whose contributions continue to inspire Ritsumeikan students and faculty alike. Similarly, our company has a dedicated space that celebrates innovation and creativity, inspiring our team to achieve excellence in their work. TeamCcompany.com/memorialroom",
-        "floor": 2,
-        "isPanorama": true,
-        "coordinate": {lat: 34.809222192764516, lng: 135.56093787063386},
-        "title": "Wakebayashi Yasuhiro Memorial Room",
-        "url": new URL('@/assets/images/guess/panorama/p5.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
-    {
-        "description": "The OIC Library's front at Ritsumeikan is a welcoming space open to all students, offering an extensive collection of books and resources in a calm environment designed for focused study and research. Our company provides a modern library setting with collaborative spaces and advanced technology, fostering a productive learning atmosphere. TeamCcompany.com/library",
-        "floor": 2,
-        "isPanorama": true,
-        "coordinate": {lat: 34.8100662, lng: 135.5620985},
-        "title": "OIC Library Front",
-        "url": new URL('@/assets/images/guess/panorama/p6.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
-    {
-        "description": "Experience the elegance of Japanese traditional furniture at the OIC Library, where cultural heritage meets modern learning in a thoughtfully designed space that encourages calm and contemplation. Our company’s facilities blend contemporary design with comfort, creating an inspiring environment for creativity and collaboration. TeamCcompany.com/furniture",
-        "floor": 3,
-        "isPanorama": true,
-        "coordinate": {lat: 34.8100715, lng: 135.5623791},
-        "title": "OIC Library's Japanese Traditional Furniture",
-        "url": new URL('@/assets/images/guess/panorama/p7.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
-    {
-        "description": "The Machi Library at OIC provides a unique community-centered library experience, designed to connect students with local resources and encourage collaborative learning in an inviting atmosphere. Our company similarly emphasizes community engagement, offering a resource-rich environment that connects employees with local networks and promotes teamwork. TeamCcompany.com/employeelibrary",
-        "floor": 1,
-        "isPanorama": true,
-        "coordinate": {lat: 34.8100789, lng: 135.5625474},
-        "title": "Machi Library OIC",
-        "url": new URL('@/assets/images/guess/panorama/p8.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
-    {
-        "description": "The OIC Cafeteria offers a variety of fresh and affordable meals, creating a welcoming space for students to dine, socialize, and recharge between classes. Our company cafeteria goes a step further, providing a diverse menu that caters to various dietary preferences while encouraging informal networking and social interaction among employees. TeamCcompany.com/cafeteria",
-        "floor": 1,
-        "isPanorama": true,
-        "coordinate": {lat: 34.8100871, lng: 135.5615275},
-        "title": "OIC Cafeteria",
-        "url": new URL('@/assets/images/guess/panorama/p9.jpeg', import.meta.url).href,
-        "haov": 240,
-        "vaov": 75
-    },
+  {
+    description:
+      'The fire hydrant near the staircase on the H5 floor sits beside large windows, allowing natural light to fill the space. It combines functionality with a sense of openness. Our company office also has comfortable spaces open to public and safety equipments. You can check it here: TeamCcompany.com',
+    floor: 5,
+    isPanorama: true,
+    coordinate: { lat: 34.8093582, lng: 135.5615536 },
+    title: 'The fire hydrant and window in front of the stairs',
+    url: new URL('@/assets/images/guess/panorama/p1.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
+  {
+    description:
+      'This outdoor area near Building H offers a peaceful open space surrounded by greenery, where students can take a break and enjoy the fresh air. Our company also values open outdoor spaces, providing similar relaxing environments at our offices. You can check it here: TeamCcompany.com',
+    floor: 1,
+    isPanorama: true,
+    coordinate: { lat: 34.80898639844242, lng: 135.562220669336 },
+    title: 'Outside',
+    url: new URL('@/assets/images/guess/panorama/p10.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
+  {
+    description:
+      'The view between the C and H buildings on the H4 floor showcases the unique layout of the campus, with greenery and open walkways that invite relaxation and reflection. Our company spaces also feature open, scenic areas for visitors to unwind. You can check it here : TeamCcompany.com',
+    floor: 4,
+    isPanorama: true,
+    coordinate: { lat: 34.809481, lng: 135.5616153 },
+    title: 'In front of the laboratory and a painting',
+    url: new URL('@/assets/images/guess/panorama/p2.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
+  {
+    description:
+      'The corridor outside the research rooms on C4 is adorned with colorful paintings, creating an inspiring and creative environment for learning and collaboration. Our office halls are also decorated with art pieces that inspire our team every day. You can check it here: TeamCcompany.com',
+    floor: 4,
+    isPanorama: true,
+    coordinate: { lat: 34.8100302, lng: 135.5614118 },
+    title: 'In front of Arena',
+    url: new URL('@/assets/images/guess/panorama/p3.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
+  {
+    description:
+      'Ritsumeikan’s C2 Arena is a versatile space where students engage in physical education classes, club practices, and personal recreational activities. A hub for active learning and community building. In comparison, our company offers state-of-the-art facilities that support diverse recreational activities and team-building events. TeamCcompany.com/arena',
+    floor: 2,
+    isPanorama: true,
+    coordinate: { lat: 34.8098504, lng: 135.5610534 },
+    title: 'In Front of Arena',
+    url: new URL('@/assets/images/guess/panorama/p4.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
+  {
+    description:
+      'The Wakebayashi Yasuhiro Memorial Room honors the life and work of Professor Wakebayashi Yasuhiro, a dedicated scholar whose contributions continue to inspire Ritsumeikan students and faculty alike. Similarly, our company has a dedicated space that celebrates innovation and creativity, inspiring our team to achieve excellence in their work. TeamCcompany.com/memorialroom',
+    floor: 2,
+    isPanorama: true,
+    coordinate: { lat: 34.809222192764516, lng: 135.56093787063386 },
+    title: 'Wakebayashi Yasuhiro Memorial Room',
+    url: new URL('@/assets/images/guess/panorama/p5.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
+  {
+    description:
+      "The OIC Library's front at Ritsumeikan is a welcoming space open to all students, offering an extensive collection of books and resources in a calm environment designed for focused study and research. Our company provides a modern library setting with collaborative spaces and advanced technology, fostering a productive learning atmosphere. TeamCcompany.com/library",
+    floor: 2,
+    isPanorama: true,
+    coordinate: { lat: 34.8100662, lng: 135.5620985 },
+    title: 'OIC Library Front',
+    url: new URL('@/assets/images/guess/panorama/p6.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
+  {
+    description:
+      'Experience the elegance of Japanese traditional furniture at the OIC Library, where cultural heritage meets modern learning in a thoughtfully designed space that encourages calm and contemplation. Our company’s facilities blend contemporary design with comfort, creating an inspiring environment for creativity and collaboration. TeamCcompany.com/furniture',
+    floor: 3,
+    isPanorama: true,
+    coordinate: { lat: 34.8100715, lng: 135.5623791 },
+    title: "OIC Library's Japanese Traditional Furniture",
+    url: new URL('@/assets/images/guess/panorama/p7.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
+  {
+    description:
+      'The Machi Library at OIC provides a unique community-centered library experience, designed to connect students with local resources and encourage collaborative learning in an inviting atmosphere. Our company similarly emphasizes community engagement, offering a resource-rich environment that connects employees with local networks and promotes teamwork. TeamCcompany.com/employeelibrary',
+    floor: 1,
+    isPanorama: true,
+    coordinate: { lat: 34.8100789, lng: 135.5625474 },
+    title: 'Machi Library OIC',
+    url: new URL('@/assets/images/guess/panorama/p8.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
+  {
+    description:
+      'The OIC Cafeteria offers a variety of fresh and affordable meals, creating a welcoming space for students to dine, socialize, and recharge between classes. Our company cafeteria goes a step further, providing a diverse menu that caters to various dietary preferences while encouraging informal networking and social interaction among employees. TeamCcompany.com/cafeteria',
+    floor: 1,
+    isPanorama: true,
+    coordinate: { lat: 34.8100871, lng: 135.5615275 },
+    title: 'OIC Cafeteria',
+    url: new URL('@/assets/images/guess/panorama/p9.jpeg', import.meta.url).href,
+    haov: 240,
+    vaov: 75,
+  },
 ]
 
-export default imageData;
+export default imageData
