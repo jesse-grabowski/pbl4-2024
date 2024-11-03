95edeca586219bdbbfbbcb8aaba714185b72c0a3
genkimats <genkimatsu24@gmail.com>
2024-11-03 21:59:30 +0900

    changed leaderboard id

--- src/data/leaderboard-credential.ts
+++ src/data/leaderboard-credential.ts
@@ -1,4 +1,4 @@
-const id = 'tpbH0M4HiGifjDCgz6Qc';
+const id = 'tpbH0M4HiGifjDCgz6Qd';
 
 export const LeaderboardCredential = {
     url: `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`
